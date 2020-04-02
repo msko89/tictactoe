e5ef21e2d2304d6716ce1bc707b934dcab8b841a
msko89 <kms89123@gmail.com>
2020-04-02 11:52:29 +0900

    Updates

---
+++ precache-manifest.5366911e35b83b5055a511b22b218cd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39974b8d235fde479cb6477025aa13e",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "816cb7d6200bf881db7d",
+    "url": "/tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "8c68f0e50eb257e55eae",
+    "url": "/tictactoe/static/js/2.28d84745.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.28d84745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816cb7d6200bf881db7d",
+    "url": "/tictactoe/static/js/main.b0676037.chunk.js"
+  },
+  {
+    "revision": "2e12c6d14e8045a44b3e",
+    "url": "/tictactoe/static/js/runtime-main.c1ec8090.js"
+  }
+]);
